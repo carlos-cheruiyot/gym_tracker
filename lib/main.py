1a079bf9aa6eba34fdcbfcac0be2892f86a85451
carlos <carloskip25@gmail.com>
2025-06-05 00:20:41 +0300

    ssetting up the entry point cli

---
+++ lib/main.py
@@ -0,0 +1,10 @@
+# main.py
+from models import create_tables
+
+def main():
+    create_tables()
+    print("Welcome to Gym Tracker!")
+    # CLI menu will go here
+
+if __name__ == "__main__":
+    main()
